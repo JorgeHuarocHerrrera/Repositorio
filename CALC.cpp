4b4d4ebb7036b3074cb7b5e89b4c35891c163b09
Frogout2022 <98139737+Frogout2022@users.noreply.github.com>
2022-01-21 00:15:19 -0500

    Create CALC.cpp

---
+++ CALC.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	cout<<"CALCULADORA";
+	cout<<"1. SUMAR";
+	cout<<"2. RESTAR";
+	cout<<"3. MULTIPLICAR";
+	cout<<"4. DIVIDIR";
+	cout<<"5. SALIR";
+	return 0;
+}
